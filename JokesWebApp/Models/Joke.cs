8bc20aa63fb9349237318771ff4924a64c47b6f1
Jose1 <Jose1@LAPTOP-B0IQ9O83>
2022-03-13 18:09:25 -0400

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        //Properties, default type of project property
+        //Getter and setter, get; set; in C#
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //Creating method constructor, ctor
+        public Joke()
+        {
+
+        }
+    }
+}
